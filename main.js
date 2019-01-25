0ebfbc6020605d8a46bfba66b6d9d1885ff10d66
kcvpll <korsun.marketing@gmail.com>
2019-01-26 01:37:46 +0500

    radiobutton

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-
+/* 
 function fun1(){
     var checkbox;
     checkbox = document.getElementById ('one');
@@ -6,5 +6,14 @@ function fun1(){
         alert('Выбран');
     } else {
         alert('Не выбран!');
+    }
+} */
+
+function fun1(){
+    var radi=document.getElementsByName('r1');
+    for (var i=0; i< radi.length; i++){
+        if (radi[i].checked){
+            alert('Выбран ' +i+ ' элемент');
+        }
     }
 }
