020d88174244d2aef99841647089c766704976f9
kcvpll <korsun.marketing@gmail.com>
2019-01-26 01:46:26 +0500

    select

--- main.js
+++ main.js
@@ -1,19 +1,5 @@
-/* 
 function fun1(){
-    var checkbox;
-    checkbox = document.getElementById ('one');
-    if (checkbox.checked){
-        alert('Выбран');
-    } else {
-        alert('Не выбран!');
-    }
-} */
-
-function fun1(){
-    var radi=document.getElementsByName('r1');
-    for (var i=0; i< radi.length; i++){
-        if (radi[i].checked){
-            alert('Выбран ' +i+ ' элемент');
-        }
-    }
+    var sel = document.getElementById('mySelect').selectedIndex;
+    var options = document.getElementById('mySelect').options;
+    alert ('Выбрана опция ' + options[sel].text)
 }
